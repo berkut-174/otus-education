36aefff44e684cf6f154dc79809f8acf1531bc57
Alexander Shemetov <berkut_174@mail.ru>
2021-11-30 14:16:03 +0500

    lesson 01, 02, 03, 04, 05

---
+++ 01-kernel/packer/scripts/stage-1-kernel-update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Import the public key
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+# Install elrepo
+yum install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm -y
+# Install new kernel
+yum --enablerepo elrepo-kernel install kernel-ml kernel-ml-devel -y
+# Remove older kernels (Only for demo! Not Production!)
+rm -f /boot/*3.10*
+# Update GRUB
+grub2-mkconfig -o /boot/grub2/grub.cfg
+grub2-set-default 0
+echo "Grub update done."
+# Reboot VM
+shutdown -r now
---
+++ 05-nfs/nfss_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+yum install -y nfs-utils
+
+systemctl enable --now firewalld
+
+firewall-cmd --add-service="nfs3" \
+    --add-service="rpc-bind" \
+    --add-service="mountd" \
+    --permanent
+
+firewall-cmd --reload
+
+mkdir -p /srv/share/upload
+chown -R nfsnobody:nfsnobody /srv/share
+chmod 0777 /srv/share/upload
+
+echo '/srv/share 192.168.50.11/32(rw,sync,root_squash)' > /etc/exports
+
+systemctl enable --now nfs
